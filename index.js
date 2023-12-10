ea75194ef251be9be6bf81ea7ce4d51c58b129af
AGUSTIN <cristian-stolzenbach@hotmail.com>
2023-12-10 09:17:27 -0300

    todo de nuevo

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-// comentando let me falto eso  y  creo que por eso no funciona  (no se)
-let precio = 400000;
-let precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio;
+
+
+// no funciono nada por aca creo que es mas practica y en clase se enseño por html-------------------------------------------------
+// {comentando let me falto eso  y  creo que por eso no funciona  (no se)
+//let precio = 400000;
+//let precioSpan = document.querySelector(".precio-inicial");
+//precioSpan.innerHTML = precio;
 // programar aca boton mas
-cantidadAumentada = Number(cantidad.innerHTML);
-cantidadAumentada = cantidadAumentada + 1;
-cantidad.innerHTML = cantidadAumentada;
-valor = document.querySelector(".valor-total");
-if (cantidadAumentada <= 0) {
-  valor.innerHTML = 0;
-} else {
-  valor.innerHTML = cantidadAumentada * precio;
-}
+//cantidadAumentada = Number(cantidad.innerHTML);
+//cantidadAumentada = cantidadAumentada + 1;
+//cantidad.innerHTML = cantidadAumentada;
+//valor = document.querySelector(".valor-total");
+//if (cantidadAumentada <= 0) {
+//  valor.innerHTML = 0;
+//} else {
+//  valor.innerHTML = cantidadAumentada * precio;
+//}
 
 // boton menos
 
-cantidadReducida = Number(cantidad.innerHTML);
-cantidadReducida = cantidadReducida - 1;
-cantidad.innerHTML = cantidadReducida;
-valor = document.querySelector(".valor-total");
-if (cantidadReducida <= 0) {
-  valor.innerHTML = 0;
-} else {
-  valor.innerHTML = cantidadReducida * precio;
-}
+//cantidadReducida = Number(cantidad.innerHTML);
+//cantidadReducida = cantidadReducida - 1;
+//cantidad.innerHTML = cantidadReducida;
+//valor = document.querySelector(".valor-total");
+//if (cantidadReducida <= 0) {
+//  valor.innerHTML = 0;
+//} else {
+//  valor.innerHTML = cantidadReducida * precio;
+//}
