6b45b65ee1f980fe7aefb6bf285b9768669b7065
AGUSTIN <cristian-stolzenbach@hotmail.com>
2023-12-10 08:54:21 -0300

    programando el boton menos y comentando let

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-precio = 400000;
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio;
+// comentando let me falto eso  y  creo que por eso no funciona  (no se)
+let precio = 400000;
+let precioSpan = document.querySelector(".precio-inicial");
+let precioSpan.innerHTML = precio;
 // programar aca boton mas
 cantidadAumentada = Number(cantidad.innerHTML);
 cantidadAumentada = cantidadAumentada + 1;
@@ -8,7 +9,18 @@ cantidad.innerHTML = cantidadAumentada;
 valor = document.querySelector(".valor-total");
 if (cantidadAumentada <= 0) {
   valor.innerHTML = 0;
-} 
-else {
+} else {
   valor.innerHTML = cantidadAumentada * precio;
+}
+
+// boton menos
+
+cantidadReducida = Number(cantidad.innerHTML);
+cantidadReducida = cantidadReducida - 1;
+cantidad.innerHTML = cantidadReducida;
+valor = document.querySelector(".valor-total");
+if (cantidadReducida <= 0) {
+  valor.innerHTML = 0;
+} else {
+  valor.innerHTML = cantidadReducida * precio;
 }
