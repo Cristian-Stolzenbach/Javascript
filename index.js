adc82b3421d634ae1017a9c905c1b52c73bcf28f
AGUSTIN <cristian-stolzenbach@hotmail.com>
2023-12-10 08:28:40 -0300

    programacion de boton mas

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-precio = 400000
-
+precio = 400000;
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
+// programar aca
+cantidadAumentada = Number(cantidad.innerHTML);
+cantidadAumentada = cantidadAumentada + 1;
+cantidad.innerHTML = cantidadAumentada;
+valor = document.querySelector(".valor-total");
+if (cantidadAumentada <= 0) {
+  valor.innerHTML = 0;
+} 
+else {
+  valor.innerHTML = cantidadAumentada * precio;
+}
